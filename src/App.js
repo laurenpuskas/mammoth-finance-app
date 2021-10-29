ac2fdab5ba006b374f18f7a003684bdac745d32b
Liliripe <madebyrossi@gmail.com>
2021-10-29 17:06:48 -0500

    init api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const frontendUrl =
+  'https://api.github.com/repos/laurenpuskas/mammoth-frontend/issues?state=all';
+const backendUrl =
+  'https://api.github.com/repos/laurenpuskas/mammoth-backend/issues?state=all';
+
+const headers = {
+  headers: {
+    Accept: 'application/vnd.github.v3+json',
+  },
+};
+
+/**
+ * Fetch all issues from both repositories
+ */
 
 function App() {
+  // manage state for issues
+  const [issues, setIssues] = useState([]);
+
+  // fetch all issues
+  useEffect(() => {
+    async function fetchIssues() {
+      try {
+        const [frontendData, backendData] = await Promise.all([
+          fetch(frontendUrl, headers).then((res) => {
+            return res.json();
+          }),
+          fetch(backendUrl, headers).then((res) => {
+            return res.json();
+          }),
+        ]);
+        const allData = [...frontendData, ...backendData];
+        console.log(allData);
+        setIssues(allData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchIssues();
+  }, []);
+
+  if (!issues.length) return <div>No issues found.</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {issues.map((issue, index) => {
+        return <div key={index}>{issue.url}</div>;
+      })}
+    </>
   );
 }
 
